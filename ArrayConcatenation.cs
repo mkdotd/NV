0f4a6b8db93a155acf02d9a18c0be9e85107d466
mkdotd <madhan.kmr@outlook.com>
2025-07-27 18:31:29 +0300

    concatenation

---
+++ ArrayConcatenation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ArrayConcatenation
+    {
+        public static int[] ArrayConcatenations(int[] nums)
+        {
+            int k = nums.Length;
+            int[] ans = new int[ k * 2];
+            for (int i = 0; i < k; i++)
+            {
+                ans[i] = nums[i];
+                ans[i+k] = nums[i];
+            
+            }
+
+            return ans;
+        }        
+    }
+}
