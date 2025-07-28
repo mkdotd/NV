54b44d3eba874ea81d1750d52b5c4b90a984e8fd
mkdotd <madhan.kmr@outlook.com>
2025-07-28 20:47:35 +0300

    valid-opening-and-closing-characters

---
+++ IsValid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class IsValid
+    {
+        public bool IsValidS(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s) {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' && (stack.Count == 0 || stack.Pop() != '(')) { return false; }
+                else if (c == ']' && (stack.Count == 0 || stack.Pop() != '[')) { return false; }
+                else if (c == '}' && (stack.Count == 0 || stack.Pop() != '{')) { return false; }
+            }
+            return stack.Count == 0;
+        }        
+    }
+}
