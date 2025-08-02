3f9baf394b0a85fa0e6769af7c4a26466b8ec6f4
mkdotd <madhan.kmr@outlook.com>
2025-08-02 19:55:28 +0300

    rething

--- SingleLinkedListReverse.cs
+++ SingleLinkedListReverse.cs
@@ -35,35 +35,7 @@ namespace Arrays
         public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
             ListNode firstNode;
-            if(list1 == null) 
-                firstNode = list2;
-            else if(list2 == null)
-                firstNode = list1;
-            else
-            {
-                firstNode = list1.val <= list2.val ? list1 : list2;
-            }
-
-            while ((list1 != null && list2 != null))
-            {
-
-                var tempList1 = list1.next;
-                var tempList2 = list2.next;
-
-                if (list1.val <= list2.val)
-                {
-
-                    list1.next = list2;
-                    list1 = tempList1;
-                }
-                else
-                {
-                    list2.next = list1;
-                    list2 = tempList2;
-                }
-
-            };
-
+            
 
 
             return firstNode;
