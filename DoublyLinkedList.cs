a38fd7faee5259e2dc8c81356e4ff0bb22079b60
mkdotd <madhan.kmr@outlook.com>
2025-08-09 17:31:26 +0300

    First pass

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,39 +9,134 @@ namespace Arrays
 {
     public class DoublyLinkedList
     {
-        public DoublyLinkedList Prev { get; set; }
-        public int Value { get; set; }
-        public DoublyLinkedList Next { get; set; }
-        private DoublyLinkedList head;
-        private DoublyLinkedList tail;
+        private class Node
+        {
+            public Node(int value)
+            {
+                Value = value;
+                Next = null;
+                Prev = null;
+            }
+            public Node Prev { get; set; }
+            public int Value { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node head;
+        private Node tail;
+        private int length;
+
         public DoublyLinkedList()
         {
-
+            length = 0;
         }
 
         public int Get(int index)
         {
-
+            if(index < 0 || index >= length)
+            {
+                return -1; // Index out of bounds
+            }
+            return GetNodeAtIndex(index).Value;
         }
 
         public void AddAtHead(int val)
         {
-
+            var nodeToInsert = new Node(val);
+            if (head == null)
+            {
+                head = tail = nodeToInsert;
+            }
+            else
+            {
+                nodeToInsert.Next = head;
+                head.Prev = nodeToInsert;
+                head = nodeToInsert;
+            }
+            length++;
         }
 
         public void AddAtTail(int val)
         {
-
+            var nodeToInsert = new Node(val);
+            if (head == null)
+            {
+                head = tail = nodeToInsert;
+            }
+            else
+            {
+                nodeToInsert.Prev = tail;
+                tail.Next = nodeToInsert;
+                tail = nodeToInsert;
+            }
+            length++;
         }
 
         public void AddAtIndex(int index, int val)
         {
+            if (index == length)
+            {
+                AddAtTail(val);
+                return;
+            }
+            else if (index > length)
+            {
+                return;
+            }
+            var pointer = GetNodeAtIndex(index);
+            var nodeToInsert = new Node(val);
+            var prevNode = pointer.Prev;
+
+            prevNode.Next = nodeToInsert;
+            nodeToInsert.Prev = prevNode;
+            nodeToInsert.Next = pointer;
+            pointer.Prev = nodeToInsert;
 
         }
 
         public void DeleteAtIndex(int index)
         {
+            if (index == length && tail != null)
+            {
+                tail = tail.Prev;
+                if (tail != null)
+                {
+                    tail.Next = null;
+                }
+                return;
+            }
+            else if (index == 0 && head != null)
+            {
+                head = head.Next;
+                if (head != null)
+                {
+                    head.Prev = null;
+                }
 
+                return;
+            }
+            var pointer = GetNodeAtIndex(index);
+            if (pointer != null)
+            {
+                var prevNode = pointer.Prev;
+                var nextNode = pointer.Next;
+
+                pointer.Prev = null;
+                pointer.Next = null;
+                prevNode.Next = nextNode;
+                nextNode.Prev = prevNode;
+            }
+        }
+
+        private Node GetNodeAtIndex(int index)
+        {
+            Node pointer = head;
+            int pointerCounter = 0;
+            while (pointerCounter < index)
+            {
+                pointer = pointer.Next;
+            }
+            return pointer;
         }
     }
 }
