1bce43986445736c3a7b85d76ca026f58ef5a007
Madhan <madhan.kmr@outlook.com>
2025-08-24 20:58:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,35 +3,7 @@ using Arrays;
 using System;
 using System.Runtime.CompilerServices;
 
-//var browserHistory = new BrowserHistory("leetcode.com");
-//browserHistory.Visit("google.com");       // You are in "leetcode.com". Visit "google.com"
-//browserHistory.Visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
-//browserHistory.Visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
-//browserHistory.Back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-//browserHistory.Back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
-//browserHistory.Forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
-//browserHistory.Visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
-//browserHistory.Forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
-//browserHistory.Back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-//browserHistory.Back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
 
-//var dl = new DoublyLinkedList();
-//dl.AddAtHead(2);
-//dl.DeleteAtIndex(1);
-//dl.AddAtHead(2);
-//dl.AddAtHead(7);
-//dl.AddAtHead(3);
-//dl.AddAtHead(2);
-//dl.AddAtHead(5);
-//dl.AddAtTail(5);
-//dl.Get(5);
-//dl.DeleteAtIndex(6);
-//dl.DeleteAtIndex(4);
-
-
-//var result = Solution.MergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))), new ListNode(1, new ListNode(3, new ListNode(5))));
-//var result = Solution.MergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))), new ListNode(1, new ListNode(3, new ListNode(5))));
-//var result = Solution.ReverseList(new ListNode());
 return;
 
 Console.WriteLine("Enter array size");
